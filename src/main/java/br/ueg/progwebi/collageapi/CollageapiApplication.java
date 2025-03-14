7a6f5fc472e9e0e7ba430a3cba86e64b54d366ab
Milenna <milennaluz.dev@gmail.com>
2025-03-14 20:41:40 -0300

    versal inicial

---
+++ src/main/java/br/ueg/progwebi/collageapi/CollageapiApplication.java
@@ -0,0 +1,13 @@
+package br.ueg.progwebi.collageapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollageapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollageapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ueg/progwebi/collageapi/CollageapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ueg.progwebi.collageapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollageapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
